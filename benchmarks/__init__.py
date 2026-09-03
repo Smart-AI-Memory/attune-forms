96689ac2148a6fb485945365ff6f730254c6fd39
Patrick Roebuck <patrick.roebuck@smartaimemory.com>
2026-09-03 18:17:51 -0400

    benchmarks: add neutral benchmark package

---
+++ benchmarks/__init__.py
@@ -0,0 +1,5 @@
+"""Vendor-neutral interaction benchmark support.
+
+This package intentionally does not import attune_forms. The product is one
+possible typed-interaction adapter under test, not part of the benchmark core.
+"""
